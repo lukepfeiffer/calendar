354a01bf2a9b57d4e3f65d370f7e1d3c6ca49ca4
Hashrocket Workstation <dev@hashrocket.com>
2014-01-30 18:30:53 -0500

    User views month

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,27 @@ class PagesController < ApplicationController
   def home
     @user = User.new
   end
+
+  def calendar
+    @active_month = active_day.month
+    @active_day = active_day
+    @month =  Month.new(@active_day)
+  end
+
+  def active_day
+    date_from_params
+  end
+
+  def date_from_params
+    Date.new(year.to_i, month.to_i)
+  end
+
+  def year
+    params[:year] || Date.today.year
+  end
+
+  def month
+    params[:month] || Date.today.month
+  end
+
 end
