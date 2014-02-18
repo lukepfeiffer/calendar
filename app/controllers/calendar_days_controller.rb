e7962169a82f24c8b8b589a905eccdde76f45dec
Hashrocket Workstation <dev@hashrocket.com>
2014-02-18 18:50:57 -0500

    User adds event to day

--- app/controllers/calendar_days_controller.rb
+++ app/controllers/calendar_days_controller.rb
@@ -3,6 +3,20 @@ class CalendarDaysController < ApplicationController
     @calendar_day = find_calendar_day
   end
 
+  def destroy
+    CalendarDay.find(params[:id]).destroy
+    redirect_to calendar_path
+  end
+
+  def update
+   @calendar_day =  CalendarDay.new(calendar_day_params)
+   if @calendar_day.save
+     redirect_to calendar_path
+   else
+     redirect_to calendar_day_path
+   end
+  end
+
   def create
    @calendar_day =  CalendarDay.new(calendar_day_params)
    if @calendar_day.save
